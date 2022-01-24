989f247402f06c7be47d0c15051486d96bc13327
vrmelican <vrmelican@wpi.edu>
2022-01-23 23:39:06 -0500

    Finished d3

--- r-ggplot/ggplot.R
+++ r-ggplot/ggplot.R
@@ -1,5 +1,4 @@
 library(ggplot2)
 
 data = read.csv("../cars-sample.csv")
-data$WeightScale = data$Weight/2
 ggplot(data, aes(x=Weight, y=MPG, colour=Manufacturer, size=Weight)) + geom_point(alpha = 0.5)
