e3ec302d4019303482b8bb7c1710701fc18a2a78
Travis <travisthomps0n@hotmail.com>
2022-01-24 14:27:18 -0500

    testing font file on pages

--- graphs/threejs/src/script.js
+++ graphs/threejs/src/script.js
@@ -174,7 +174,7 @@ const scene = new THREE.Scene()
 const fontLoader = new FontLoader()
 
 fontLoader.load(
-    '/a2-DataVis-5Ways/graphs/threejs/dist/fonts/helvetiker_regular.typeface.json',
+    './fonts/helvetiker_regular.typeface.json',
     (font) =>
     {
         const fastTextCreator = (text) =>
