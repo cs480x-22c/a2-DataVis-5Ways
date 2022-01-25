fc4394ade1c67980b0ad40bed7773fe96c41fec5
Travis <travisthomps0n@hotmail.com>
2022-01-25 14:22:48 -0500

    d3 graph

--- graphs/d3/src/script.js
+++ graphs/d3/src/script.js
@@ -1,73 +1,81 @@
-import './style.css'
-import * as d3 from 'd3'
-import txt from 'raw-loader!./cars-sample.csv';
+import "./style.css"
+import * as d3 from 'D3'
 
+// Starter Code https://www.d3-graph-gallery.com/graph/scatter_grouped.html
 /**
- * Car Data
+ * Graph Sizes
  */
-function Car(name, manufacturer, mpg, weight)
-{
-    this.name = name
-    this.manufacturer = manufacturer
-    this.mpg = mpg
-    this.weight = weight
+
+const margin = {
+    top: 10,
+    right: 30,
+    bottom: 30,
+    left: 60
 }
 
-let cars = []
-
-// Parse Car Data
-
-txt.replaceAll("\"", "")
-    .split("\n")
-    .forEach(car =>
-    {
-        const rawCar = car.split(",")
-        const newCar = new Car(rawCar[1], rawCar[2], +rawCar[3], +rawCar[7])
-        if(!isNaN(newCar.mpg) || !isNaN(newCar.weight))
-            cars.push(newCar)
-    })
-
-/**
- * Createing SVG
- */
-
-var margin = {top: 10, right: 20, bottom: 30, left: 50},
-    width = 500 - margin.left - margin.right,
-    height = 420 - margin.top - margin.bottom;
-
-const svg = d3.select("body").append("svg")
-.attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")")
-
-d3.csv("./cars-sample.csv", (data) => {
-    console.log(data)
-})
-
-/*
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: 800 - margin.left - margin.right,
+    height: 600 - margin.top - margin.bottom
 }
 
-// Handling Resizing
-window.addEventListener("resize", () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+/**
+ * Creating Graph
+ */
 
-    svg.attr("width", sizes.width)
-        .attr("height", sizes.height)
+let svg = d3.select("#cars-chart")
+            .append("svg")
+                .attr("width", sizes.width + margin.left + margin.right)
+                .attr("height", sizes.height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")")
 
-    render()
-})
-
-const svg = d3.select("body").append("svg")
-    .attr("width", sizes.width)
-    .attr("height", sizes.height)
-*/
 
 /**
- * Shapes
+ * Loading Car Data into Graph
+ */
+
+d3.csv("./cars-sample.csv").then(data =>
+{
+    // Removing incomplete data
+    data = data.filter(data => (!isNaN(data.MPG) && !isNaN(data.Weight)))
+    
+    // Creating Axis'
+    let x = d3.scaleLinear()
+        .domain([1500, 5100])
+        .range([0, sizes.width])
+
+    // Creating x axis and moving it
+    svg.append("g")
+        .attr("transform", "translate(0," + sizes.height + ")")
+        .call(d3.axisBottom(x))
+
+    let y = d3.scaleLinear()
+        .domain([7, 43])
+        .range([sizes.height, 0])
+
+    // Creating y axis
+    svg.append("g")
+        .call(d3.axisLeft(y))
+
+    const color = d3.scaleOrdinal()
+        .domain(["bmw", "ford", "honda", "mercedes", "toyota"])
+        .range(["#FA252580", "#939E0080", "#44E39380", "#29E7F580" , "#F73FFA80"])
+
+    svg.append("g")
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", (d) => { return x(d.Weight)})
+            .attr("cy", (d) => { return y(d.MPG)})
+            .attr("r", (d) => {return d.Weight / 300})
+            .style("fill", (d) => { return color(d.Manufacturer)})
+
+})
+
+/**
+ * Learning Notes
+ * 
+ * appending g in the svg is so you can group all the elements
  */
--- graphs/threejs/readme.md
+++ graphs/threejs/readme.md
@@ -1,6 +1,3 @@
 To create my graphs I used the D3, Threejs, 
 
-To run the Threejs, you need to have node installed and move to the threejs folder in your terminal. Then run the following commands
-- npm install
-- npm run dev
-This is due to coors errors from the files if you do not.
+https://travisthomp.github.io/a2-DataVis-5Ways/graphs/threejs/dist/index.html
