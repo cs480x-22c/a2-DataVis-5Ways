0ea5328d3f45d007525103e9bb4c216e640bb759
jacobadamsky <98067374+jacobadamsky@users.noreply.github.com>
2022-01-25 20:16:23 -0500

    Update ggplot2.R

--- code/ggplot2.R
+++ code/ggplot2.R
@@ -7,5 +7,5 @@ ggplot(csv_data, aes(Weight, MPG)) +
   geom_point(size = ifelse(csv_data[,8]>4000, 8,
                            ifelse(csv_data[,8]>3000, 6,
                                   ifelse(csv_data[,8]>2000, 4, 2))),
-             color = csv_data[,1]),
+             color = csv_data[,3]),
              alpha = 0.5)
