220a5be11849852000fb4bbe0aeda1f8a8b42d39
jacobadamsky <98067374+jacobadamsky@users.noreply.github.com>
2022-01-28 01:20:36 -0500

    Update matlabvis.m

--- code/matlabvis.m
+++ code/matlabvis.m
@@ -5,23 +5,8 @@ col3 = inputTable(:, 8);
 rows = size(inputTable,1);
 
 colorMap = [zeros(rows, 1), zeros(rows, 1), ones(rows,1)];
-sizeMap = [zeros(rows, 1)];
 
 for k= 1: rows
-    if(col3{k,1} > 4000)
-        sizeMap(k, :) = 160;
-    else
-        if(col3{k,1} > 3000)
-            sizeMap(k, :) = 120;
-        else
-            if(col3{k,1} > 2000)
-                sizeMap(k, :) = 80;
-            else
-                sizeMap(k, :) = 40;
-            end
-        end
-    end
-
     if col1{k,1} == "ford"
         colorMap(k, :) = [0,0,0];
     else 
@@ -41,8 +26,7 @@ for k= 1: rows
     end
 end
 
-scatter(inputTable.Weight, inputTable.MPG, sizeMap, colorMap, 'filled',...
+scatter(inputTable.Weight, inputTable.MPG, inputTable.Weight / 40, colorMap, 'filled',...
     'MarkerFaceAlpha',.7,'MarkerEdgeAlpha',.7);
-grid on;
 xlabel(inputTable.Properties.VariableNames{8});
 ylabel(inputTable.Properties.VariableNames{3});
