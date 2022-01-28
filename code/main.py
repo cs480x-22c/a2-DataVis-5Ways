1727dd9d2e6c27545bdbf63ee5d611910751da77
jacobadamsky <98067374+jacobadamsky@users.noreply.github.com>
2022-01-28 01:22:31 -0500

    Update main.py

--- code/main.py
+++ code/main.py
@@ -6,17 +6,18 @@ h = 3
 d = 280
 cmap = plt.get_cmap('viridis')
 plt.figure(figsize=(w, h), dpi=d)
-data = np.genfromtxt("cars-sample.csv", names=True, dtype="float", delimiter=",")
-names = data["Manufacturer"]
+iris_data = np.genfromtxt("cars-sample.csv", names=True, dtype="float", delimiter=",")
+names = iris_data["Manufacturer"]
 colors = cmap(np.linspace(0, 1, len(names)))
 
-for i in range(data.size):
-    plt.plot(data["MPG"][i], data["Weight"][i], "o",
-             markersize=(data["Weight"][i]/1000).__round__(), color=colors[i],
-             label=data["Manufacturer"][i])
+for i in range(iris_data.size):
+    plt.plot(iris_data["Weight"][i], iris_data["MPG"][i], "o",
+             markersize=(iris_data["Weight"][i]/1000), color=colors[i],
+             label=iris_data["Manufacturer"][i],
+             alpha=0.5)
 
 plt.subplots_adjust(bottom=0.15, left=0.2)
 plt.xlabel("Weight")
 plt.ylabel("MPG")
-
+plt.grid()
 plt.savefig("out.png")
