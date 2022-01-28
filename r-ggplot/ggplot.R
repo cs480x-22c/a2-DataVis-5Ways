07b0c520575ad7c4422327d9bc625e51970bc2aa
vrmelican <vrmelican@wpi.edu>
2022-01-28 18:51:04 -0500

    Update

--- r-ggplot/ggplot.R
+++ r-ggplot/ggplot.R
@@ -1,4 +1,5 @@
 library(ggplot2)
 
+#need to set working directory to location of this file or it won't work
 data = read.csv("../cars-sample.csv")
 ggplot(data, aes(x=Weight, y=MPG, colour=Manufacturer, size=Weight)) + geom_point(alpha = 0.5)
