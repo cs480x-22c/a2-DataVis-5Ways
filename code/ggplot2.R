bad2e9748ce7e4a178873c0d1aa599c04f0e938e
jacobadamsky <98067374+jacobadamsky@users.noreply.github.com>
2022-01-27 23:24:01 -0500

    Update ggplot2.R

--- code/ggplot2.R
+++ code/ggplot2.R
@@ -4,8 +4,6 @@ library(tidyverse)
 csv_data = read.csv("C:/Users/docto/desktop/cars-sample.csv")
 
 ggplot(csv_data, aes(Weight, MPG)) +
-  geom_point(size = ifelse(csv_data[,8]>4000, 8,
-                           ifelse(csv_data[,8]>3000, 6,
-                                  ifelse(csv_data[,8]>2000, 4, 2))),
+  geom_point(size = Weight / 600,
              color = csv_data[,3]),
              alpha = 0.5)
