1fbb9e403743630f69253cac44c28bb87ccde807
Travis <travisthomps0n@hotmail.com>
2022-01-28 12:02:51 -0500

    python using plotly.express

--- graphs/python/bubbleplot.py
+++ graphs/python/bubbleplot.py
@@ -1,5 +1,16 @@
-# STARTER: https://www.python-graph-gallery.com/bubble-plot/
-import seaborn as sns
-import matplotlib.pyplot as plt
+# STARTER: https://plotly.com/python/bubble-charts/
+import pandas as pd
+import plotly.express as px
 
-print(plt)
+df = pd.read_csv('./data/cars-sample.csv')
+
+df['Size'] = list(map(lambda weight: weight /2000, df.Weight))
+fig = px.scatter(df, x="Weight", y="MPG",
+	         size="Size", color="Manufacturer",
+                  log_x=True, size_max=50,
+                  color_discrete_map={'bmw' : '#e0756c',
+                                      'ford': '#c3c575',
+                                      'honda': '#8dd7b7',
+                                      'mercedes': '#8fd2f4',
+                                      'toyota': '#edb0f2',})
+fig.show()
