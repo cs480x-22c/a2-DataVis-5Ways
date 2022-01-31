06969924d7e3275fb45af9c6ed2ce496ebaa4369
crelicthecleric <52921332+crelicthecleric@users.noreply.github.com>
2022-01-30 23:10:12 -0500

    added files

--- r-ggplot2, python-matplotlib, javascript-d3, tableau, excel/viscars.R
+++ r-ggplot2, python-matplotlib, javascript-d3, tableau, excel/viscars.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-mtcars_sample <- read.csv("cars-sample.csv", header=TRUE)
+mtcars_sample <- read.csv("r-ggplot2, python-matplotlib, javascript-d3, tableau, excel/javascript-d3/cars-sample.csv", header=TRUE)
 ggplot(mtcars_sample, aes(x=Weight, y=MPG)) +
   geom_point(aes(colour=Manufacturer, size=Weight), alpha=0.5)
 ggsave("img/ggplot2.png")
--- r-ggplot2, python-matplotlib, javascript-d3, tableau, excel/viscars.py
+++ r-ggplot2, python-matplotlib, javascript-d3, tableau, excel/viscars.py
@@ -5,15 +5,16 @@ import seaborn as sns
 
 def main():
     plt.style.use('ggplot')
-    mtcars = pd.read_csv("public/cars-sample.csv")
+    mtcars = pd.read_csv("javascript-d3/cars-sample.csv")
     cmap = {"bmw": "#f3b7b4",
             "ford": "#cdce8e",
             "honda": "#95d9bb",
             "mercedes": "#93d3f4",
             "toyota": "#eeb2f2"}
     _, ax = plt.subplots()
+    mtcars["Size"] = mtcars["Weight"]/100
     for key, group in mtcars.groupby("Manufacturer"):
-        group.plot.scatter(ax=ax, x="Weight", y="MPG", label=key, color=cmap[key], alpha=0.5)
+        group.plot.scatter(ax=ax, x="Weight", y="MPG", s="Size", label=key, color=cmap[key], alpha=0.5)
     plt.xlabel("Weight")
     plt.xticks(range(1000, 5000, 1000))
     plt.ylabel("MPG")
